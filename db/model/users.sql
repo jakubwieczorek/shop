54674b0526074817b2c3056802aceca254de602b
jakubwieczorek <jakub.wieczorek0101@gmail.com>
2020-09-20 12:24:51 +0200

    tablespace and users changed

--- db/model/users.sql
+++ db/model/users.sql
@@ -2,6 +2,6 @@ create user extuser identified by les6sari default tablespace tablespace_extuser
 grant connect, resource, IMP_FULL_DATABASE, EXP_FULL_DATABASE to extuser;
 GRANT UNLIMITED TABLESPACE TO extuser;
 
-create user testuser identified by les6sari default tablespace tablespace_test;
+create user testuser identified by les6sari default tablespace tablespace_testuser;
 grant connect, resource, IMP_FULL_DATABASE, EXP_FULL_DATABASE to testuser;
 grant unlimited tablespace to testuser;
